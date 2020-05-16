53a9ce4f7f951d7c69b84bd992d213adbe378404
Veikka Valtteri Kallinen <velikallinen@outlook.com>
2020-05-16 21:30:52 +0300

    Add communication with device.

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 const path = require('path');
-
-// install express: npm install --save express
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const errorController = require('./controllers/error');
 const mongoConnect = require('./utils/database').mongoConnect;
-
 const app = express();
+
 
 app.set('view engine','ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-//const spaceRoutes = require('./routes/space');
-//const exhibitionRoutes = require('./routes/exhibition');
-//const trackerRoutes = require('./routes/tracker');
+const spaceRoutes = require('./routes/space');
+const exhibitionRoutes = require('./routes/exhibition');
+const trackerRoutes = require('./routes/tracker');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/',exhibitionRoutes);
+app.use('/',exhibitionRoutes);
 app.use('/admin', adminRoutes);
-// app.use('/spaces',spaceRoutes);
-// app.use('/trackers',trackerRoutes);
+app.use('/spaces',spaceRoutes);
+app.use('/trackers',trackerRoutes);
 
+// API for the database
+app.use(bodyParser.json())
+var message = {"msg":"setup"};
+app.get('/tracker/api/', function (req, res) {
+    message = {"msg":"preview"};
+    
+});
+
+app.post('/tracker/api/', function (req, res) {
+    console.log(req.body);
+    if (req.body.msg == 'preview') {
+	message = {"msg":"setup"};
+    }
+    res.send(message);
+});
 
 app.use(errorController.get404);
 
